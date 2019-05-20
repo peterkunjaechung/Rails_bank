6d6f4e10aa64eb933eb100b0ccc9947f00feeee1
contactpeterchung.com <contactpeterchung@gmail.com>
2019-05-20 13:37:13 -0600

    First commit

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,52 @@
+class AccountsController < ApplicationController
+  before_action :set_account, only: [:show, :edit, :update]
+
+  def index
+    @accounts = current_user.accounts
+  end
+
+  def show
+  end
+
+  def new
+    @account = Account.new
+  end
+
+  def create
+    @account = current_user.accounts.new(account_params)
+    if @account.save
+      flash[:success] = "Account: #{@account.name} Created"
+      redirect_to accounts_path
+    else
+      flash[:error] = "Error #{@account.errors.full_messages.join("\n")}"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @account.update(account_params)
+      redirect_to accounts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @account.destroy
+    redirect_to accounts_path
+  end
+
+  private
+    def account_params
+      params.require(:account).permit(:name, :balance)
+    end
+
+    # don't just find by the Account model or you may potentially be able to view other users accounts
+    def set_account
+      @account = current_user.accounts.find(params[:id])
+    end
+
+end
---
+++ db/schema.rb
@@ -0,0 +1,30 @@
+ActiveRecord::Schema.define(version: 2019_05_20_170803) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "name"
+    t.integer "balance"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "accounts", "users"
+end
